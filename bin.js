f92e90682c062972f1aed3662dd5648133169b6c
k1ller13 <rpishappynow@gmail.com>
2022-03-11 14:11:49 +0800

    fix: add enpm condition

--- bin.js
+++ bin.js
@@ -8,9 +8,17 @@ if (argv.length === 0) {
   process.exit(1)
 }
 const wantedPM = argv[0]
-if (wantedPM !== 'npm' && wantedPM !== 'cnpm' && wantedPM !== 'pnpm' && wantedPM !== 'yarn') {
-  console.log(`"${wantedPM}" is not a valid package manager. Available package managers are: npm, cnpm, pnpm, or yarn.`)
-  process.exit(1)
+if (
+  wantedPM !== "npm" &&
+  wantedPM !== "cnpm" &&
+  wantedPM !== "pnpm" &&
+  wantedPM !== "yarn" &&
+  wantedPM !== "enpm"
+) {
+  console.log(
+    `"${wantedPM}" is not a valid package manager. Available package managers are: npm, cnpm, pnpm, or yarn.`
+  );
+  process.exit(1);
 }
 const usedPM = whichPMRuns()
 if (usedPM && usedPM.name !== wantedPM) {
